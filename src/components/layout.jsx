4bdd0f0ef393250ec6af7cc959b506b427eed67c
Carston Henderson <carstonhenderson@icloud.com>
2020-01-10 11:03:22 -0700

    Remove unneeded query

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-
 import Header from './header'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div className="bg-gray-900 text-white">
-        <Header siteTitle={data.site.siteMetadata.title} />
-
-        <main className="container mx-auto overflow-auto p-4">{children}</main>
-      </div>
-    )}
-  />
+  <div className="bg-gray-900 text-white">
+    <main className="container mx-auto p-4 max-w-3xl">
+      <Header siteTitle="Carston Henderson" />
+      {children}
+    </main>
+  </div>
 )
 
 export default Layout
